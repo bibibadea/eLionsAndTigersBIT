d63e6758546b59990e20e65d540fdccd5c440343
Bogdan Badea <bogdan.badea@stormbraces.com>
2015-09-01 12:37:31 +0300

    yellow

---
+++ eLionsAndTigersBIT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eLionsAndTigersBIT
+//
+//  Created by Bibi Badea on 9/1/15.
+//  Copyright © 2015 Stormbraces. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
